4bbd94448971444e5f5dab9ab24bd773de783fda
Ahme460 <136134058+Ahme460@users.noreply.github.com>
2024-05-03 01:51:15 +0300

    Update views.py

--- project/app2/views.py
+++ project/app2/views.py
@@ -61,15 +61,115 @@ def remove_from_cart(request, item_id):
 	return redirect('view_cart')
 
 
+
 def pay(request):
-      
+    if request.method == "GET":
+        return render(request,'payment.html')
+    else:
+        import requests as re
+        #your api 
+        api="ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T1RjME16QXhMQ0p1WVcxbElqb2lhVzVwZEdsaGJDSjkuR2duRjJjU0pnRThsbVpqRnVGMWdPTHlzaFdlSnpSOVBJTDFkT1RBQ3B0Z3JqckxnUmo5WU43MHZqOGlGYUdVQzZmUm5mQ2tQWDZUbHBkcUVFX3J6NUE="
+        #now we send request to get first token
+        token = re.post(
+            url="https://accept.paymob.com/api/auth/tokens",
+            json= {
+            "api_key": api
+            }
+        )
+        #get token from resposne
+        api_token = token.json().get("token", None)
 
 
-    return render(request,'payment.html')
 
-#from background_task import background
-import asyncio
+        #now collect data require from doc that based on type payment
+        paylod = {
+            "auth_token":  api_token,
+            "delivery_needed": "false",
+            "amount_cents": "100",
+            "currency": "EGP",
+            "items": [
+                {
+                    "name": "ASC1515",
+                    "amount_cents": "500000",
+                    "description": "Smart Watch",
+                    "quantity": "1"
+                },
+                { 
+                    "name": "ERT6565",
+                    "amount_cents": "200000",
+                    "description": "Power Bank",
+                    "quantity": "1"
+                }
+                ],
+            "shipping_data": {
+                
+            },
+                "shipping_details": {
+                    
+                }
+            }
+        #send req
+        response = re.post(
+            url="https://accept.paymob.com/api/ecommerce/orders",
+            json=paylod  #data
+        )
 
+        #get id form response
+        id=response.json().get('id', None)
+        
+
+        #now write data payment
+        paylod = {
+            "auth_token": api_token, # first token 
+            "amount_cents": "100", 
+            "expiration": 3600, 
+            "order_id": f"{id}", # id that i get it form response 
+            "billing_data": {
+                "apartment": "803", 
+                "email": "test@gmail.com", 
+                "floor": "42", 
+                "first_name": "Clifford", 
+                "street": "Ethan Land", 
+                "building": "8028", 
+                "phone_number": "+201064160586", 
+                "shipping_method": "PKG", 
+                "postal_code": "01898", 
+                "city": "cairo", 
+                "country": "EG", 
+                "last_name": "Nicolas", 
+                "state": "Utah"
+            }, 
+            "currency": "EGP", 
+            "integration_id": 4569633 # this get it from account paymob when add type payment
+
+            }
+        #send requests
+        response = re.post(
+            url="https://accept.paymob.com/api/acceptance/payment_keys",
+            json=paylod
+        )
+
+        token = response.json().get('token', None)
+        #get token
+        # print(token, "--------")
+
+        #now open doc and you choice type pay on based on write this payload 
+        paylod = {
+            "source": {
+                "identifier": "01010101010", 
+                "subtype": "WALLET"
+            },
+            "payment_token": token
+            }
+        response = re.post(
+            url="https://accept.paymob.com/api/acceptance/payments/pay",
+            json=paylod
+
+        )
+        #now get link redirect
+        link=response.json().get('redirect_url',None)
+        
+        return redirect(link)
 
 def pay2(request):
     if request.method == "POST":
